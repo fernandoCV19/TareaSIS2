0aa2e5c7c52b397b681065c5eb4efca1ab277b22
Alejandro20032001 <dilanantezama@gmail.com>
2021-05-01 18:22:05 -0400

    Clase estudiante Agregado

--- src/tareasis2/Estudiante.java
+++ src/tareasis2/Estudiante.java
@@ -1,14 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tareasis2;
 
-/**
- *
- * @author Windows 10
- */
+import java.sql.Date;
+
+
 public class Estudiante {
+    private final int cod_sis;
+    private String nombres;
+    private String apellidos;
+    private String carrera;
+    private int ci;
+    private Date fechaNacimiento;
+    private String ciudadNacimiento;
+    private int noCertificadoNacimiento;
+    private int noPasaporte;
+    private int noLicenciaConducir;
+    private boolean habilitado;
+
+    public Estudiante(int cod_sis, String nombres, String apellidos, String carrera, int ci, Date fechaNacimiento, String ciudadNacimiento, int noCertificadoNacimiento, int noPasaporte, int noLicenciaConducir, boolean habilitado) {
+        this.cod_sis = cod_sis;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.carrera = carrera;
+        this.ci = ci;
+        this.fechaNacimiento = fechaNacimiento;
+        this.ciudadNacimiento = ciudadNacimiento;
+        this.noCertificadoNacimiento = noCertificadoNacimiento;
+        this.noPasaporte = noPasaporte;
+        this.noLicenciaConducir = noLicenciaConducir;
+        this.habilitado = habilitado;
+    }
+
     
 }
