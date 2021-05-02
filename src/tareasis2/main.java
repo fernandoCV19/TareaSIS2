295a422022a7230a73b24752aae2b57cfbef73e3
Alejandro20032001 <dilanantezama@gmail.com>
2021-05-01 21:00:46 -0400

    insert estudiante ejecutado y funcionando

--- src/tareasis2/main.java
+++ src/tareasis2/main.java
@@ -1,9 +1,16 @@
 package tareasis2;
 
+import java.sql.Date;
 import java.sql.SQLException;
+import tareasis2.servicios.EstudianteServicios;
 
 public class main {
     public static void main(String [] args) throws SQLException, ClassNotFoundException{
         Conexion.obtener();
+        Estudiante est;
+        est = new Estudiante (201900520, "Dilan Alejandro", "Antezana Serrano", "Informatica", 9364973,
+                new Date(2001, 3, 20), "Cercado", 123, 345, 567, false);
+        EstudianteServicios es = new EstudianteServicios();
+        es.guardar(Conexion.obtener(), est);
     }
 }
--- src/tareasis2/servicios/EstudianteServicios.java
+++ src/tareasis2/servicios/EstudianteServicios.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tareasis2.servicios;
 
-/**
- *
- * @author HP
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import tareasis2.Estudiante;
+
 public class EstudianteServicios {
-    
+    private final String tabla = "estudiante";
+   public void guardar(Connection conexion, Estudiante est) throws SQLException{
+      try{
+         PreparedStatement consulta;
+         consulta = conexion.prepareStatement("INSERT INTO " + this.tabla + "(cod_sis, nombres, apellidos,"+
+                 "carrera, ci, fechaNacimiento, ciudadNacimiento, noCertificadoNacimiento, noPasaporte,"+
+                 "habilitado, noLicenciaConducir) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            consulta.setInt(1, est.getCod_sis());
+            consulta.setString(2, est.getNombres());
+            consulta.setString(3, est.getApellidos());
+            consulta.setString(4, est.getCarrera());
+            consulta.setInt(5, est.getCi());
+            consulta.setDate(6, est.getFechaNacimiento());
+            consulta.setString(7, est.getCiudadNacimiento());
+            consulta.setInt(8, est.getNoCertificadoNacimiento());
+            consulta.setInt(9, est.getNoPasaporte());
+            consulta.setBoolean(10, est.isHabilitado());
+            consulta.setInt(11, est.getNoLicenciaConducir());
+            
+            consulta.executeUpdate();
+      }catch(SQLException ex){
+         throw new SQLException(ex);
+      }
+   }
 }
