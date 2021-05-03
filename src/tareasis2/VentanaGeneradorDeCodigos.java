8bff7e939ae3ebd42dd4169661c406ccbc71d87b
alejandroxp08 <alejandroxp08@gmail.com>
2021-05-03 19:16:24 -0400

    cambios en la ventana de codigos

--- src/tareasis2/VentanaGeneradorDeCodigos.java
+++ src/tareasis2/VentanaGeneradorDeCodigos.java
@@ -33,10 +33,9 @@ public class VentanaGeneradorDeCodigos extends javax.swing.JFrame {
         String cod5 = codigo.getCodigo5();
   //------------------------------------------------------------------------------      
 
+
         panel1 = new tareasis2.panel();
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
@@ -47,8 +46,6 @@ public class VentanaGeneradorDeCodigos extends javax.swing.JFrame {
         jCod5Label = new javax.swing.JLabel();
         jCod4Label = new javax.swing.JLabel();
         jCod1Label = new javax.swing.JLabel();
-        gestionLabel = new javax.swing.JLabel();
-        jLabel9 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -56,12 +53,6 @@ public class VentanaGeneradorDeCodigos extends javax.swing.JFrame {
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         jLabel1.setText("CODIGOS DE ACCESO");
-
-        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        jLabel2.setText("CARRERA: LICENCIATURA EN ");
-
-        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        jLabel3.setText("GESTIÓN :");
 
         jLabel4.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel4.setText("COD. DE ACCESO 1  : ");
@@ -77,20 +68,21 @@ public class VentanaGeneradorDeCodigos extends javax.swing.JFrame {
 
         jLabel8.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel8.setText("COD. DE ACCESO 5  :");
+
         jCod2Label.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        jCod2Label.setText(cod2);
+        jCod2Label.setText("jLabel10");
 
         jCod3Label.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        jCod3Label.setText(cod3);
+        jCod3Label.setText("jLabel11");
 
         jCod5Label.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        jCod5Label.setText(cod5);
+        jCod5Label.setText("jLabel13");
 
         jCod4Label.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        jCod4Label.setText(cod4);
+        jCod4Label.setText("jLabel12");
 
         jCod1Label.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        jCod1Label.setText(cod1);
+        jCod1Label.setText("jLabel9");
 
         jButton1.setText("<");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -109,12 +101,7 @@ public class VentanaGeneradorDeCodigos extends javax.swing.JFrame {
                         .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addGroup(panel1Layout.createSequentialGroup()
                                 .addGap(32, 32, 32)
-                                .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jButton1)
-                                    .addGroup(panel1Layout.createSequentialGroup()
-                                        .addComponent(jLabel2)
-                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(jLabel9)))
+                                .addComponent(jButton1)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panel1Layout.createSequentialGroup()
                                 .addGap(222, 222, 222)
@@ -134,11 +121,7 @@ public class VentanaGeneradorDeCodigos extends javax.swing.JFrame {
                     .addGroup(panel1Layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addGap(9, 9, 9)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
-                .addComponent(jLabel3)
-                .addGap(47, 47, 47)
-                .addComponent(gestionLabel)
-                .addGap(51, 51, 51))
+                .addContainerGap(231, Short.MAX_VALUE))
         );
         panel1Layout.setVerticalGroup(
             panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -147,13 +130,7 @@ public class VentanaGeneradorDeCodigos extends javax.swing.JFrame {
                 .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(jButton1))
-                .addGap(29, 29, 29)
-                .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel9)
-                    .addComponent(jLabel2)
-                    .addComponent(jLabel3)
-                    .addComponent(gestionLabel))
-                .addGap(48, 48, 48)
+                .addGap(94, 94, 94)
                 .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
                     .addComponent(jCod1Label))
@@ -176,7 +153,7 @@ public class VentanaGeneradorDeCodigos extends javax.swing.JFrame {
                 .addGroup(panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel8)
                     .addComponent(jCod5Label))
-                .addContainerGap(96, Short.MAX_VALUE))
+                .addContainerGap(120, Short.MAX_VALUE))
         );
 
         getContentPane().add(panel1, java.awt.BorderLayout.CENTER);
